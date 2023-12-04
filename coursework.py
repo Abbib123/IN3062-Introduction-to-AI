952661960e74f221edde06b5fc8f4220fcac0d83
BilalBismillah <71775466+BilalBismillah@users.noreply.github.com>
2023-12-04 12:39:54 +0000

    Create coursework.py

---
+++ coursework.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 12:07:27 2023
+
+@author: bilal
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_lfw_people
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import tensorflow.keras.utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn import metrics
+import io
+import os
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
