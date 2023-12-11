d157d91aa5ec6991b071d9716ea2e3001c8f26ea
EthanBrewerCity <91800294+EthanBrewerCity@users.noreply.github.com>
2023-12-11 12:07:08 +0000

    Successfully imported all tensorflow.keras imports

--- coursework.py
+++ coursework.py
@@ -7,11 +7,11 @@ from sklearn.datasets import fetch_lfw_people
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import tensorflow.keras.utils
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn import metrics
 import io
 import os
