11c98653377cd10773fa67ea90af02c6330348ed
EthanBrewerCity <91800294+EthanBrewerCity@users.noreply.github.com>
2023-12-11 13:20:02 +0000

    Reorganised Imports for cleanlieness and to avoid confusion

--- coursework.py
+++ coursework.py
@@ -3,8 +3,11 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.datasets import fetch_lfw_people
+import io
+import os
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from tensorflow import keras
@@ -13,8 +16,5 @@ from keras.layers import Dense, Activation, Flatten, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn import metrics
-import io
-import os
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
-import seaborn as sns
