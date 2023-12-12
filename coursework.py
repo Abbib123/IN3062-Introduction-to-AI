c869b103347b4a2e52a0c32997dec0a1cbd5979f
EthanBrewerCity <91800294+EthanBrewerCity@users.noreply.github.com>
2023-12-12 13:58:47 +0000

    Added df.drop lines to remove unecessary columns from df

--- coursework.py
+++ coursework.py
@@ -28,18 +28,37 @@ print(df[0:6392])
 df = pd.read_csv(filename_read, na_values=['NA', '?'])
 
 # Strip non-numerics
-df = df.select_dtypes(include=['int', 'float'])
+#df = df.select_dtypes(include=['int', 'float'])
 
 headers = list(df.columns.values)
 fields = []
 
-for field in headers:
-    fields.append({
-        'name' : field,
-        'mean': df[field].mean(),
-        'var': df[field].var(),
-        'sdev': df[field].std()
-    })
+#for field in headers:
+#    fields.append({
+#        'name' : field,
+#        'mean': df[field].mean(),
+#        'var': df[field].var(),
+#        'sdev': df[field].std()
+#    })
 
 for field in fields:
     print(field)
+
+print(f"before drop: {df.columns}")
+df.drop('ID',axis=1, inplace=True)
+df.drop('N',axis=1, inplace=True)
+df.drop('D',axis=1, inplace=True)
+df.drop('G',axis=1, inplace=True)
+df.drop('C',axis=1, inplace=True)
+df.drop('A',axis=1, inplace=True)
+df.drop('H',axis=1, inplace=True)
+df.drop('M',axis=1, inplace=True)
+df.drop('O',axis=1, inplace=True)
+df.drop('Left-Fundus',axis=1, inplace=True)
+df.drop('Right-Fundus',axis=1, inplace=True)
+df.drop('Left-Diagnostic Keywords',axis=1, inplace=True)
+df.drop('Right-Diagnostic Keywords',axis=1, inplace=True)
+df.drop('filepath',axis=1, inplace=True)
+df.drop('target',axis=1, inplace=True)
+print(f"after drop: {df.columns}")
+print(df[0:6392])
