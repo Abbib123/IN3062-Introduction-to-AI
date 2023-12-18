db792ac579aad1e52693660aeddc16d33d0643ef
BilalBismillah <71775466+BilalBismillah@users.noreply.github.com>
2023-12-18 16:39:07 +0000

    wip: modified imports

--- coursework.py
+++ coursework.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-import numpy as np
-import tensorflow as tf
 import io
 import os
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras.utils
 import seaborn as sns
-from sklearn.datasets import fetch_lfw_people
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from tensorflow import keras
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
-from sklearn import metrics
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
 
-path = "."  
+path = "./IN3062-Introduction-to-AI"  
 
 filename_read = os.path.join(path, "full_df.csv")
 df = pd.read_csv(filename_read)
@@ -61,4 +60,6 @@ df.drop('Right-Diagnostic Keywords',axis=1, inplace=True)
 df.drop('filepath',axis=1, inplace=True)
 df.drop('target',axis=1, inplace=True)
 print(f"after drop: {df.columns}")
-print(df[0:6392])
+# print(df[0:6392])
+
+print(df.head())
